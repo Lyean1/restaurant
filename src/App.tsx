2672a27cc7658ea7a08a299e1c1961e8f988b4f3
Lyean <lyean1500@gmail.com>
2023-07-19 19:53:36 +0630

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="app">
-      <Navbar title="restaurant" />
+      <Navbar title="Restaurant" />
     </div>
   );
 };
